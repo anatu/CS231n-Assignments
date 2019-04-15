a35d88e32bf1276127e5f31e41eb1be41bfd4b4d
anatu <natu.anand@gmail.com>
2019-04-14 18:51:09 -0700

    Finished softmax except inline q2

--- assignment1/cs231n/classifiers/softmax.py
+++ assignment1/cs231n/classifiers/softmax.py
@@ -37,33 +37,31 @@ def softmax_loss_naive(W, X, y, reg):
     # Number of examples
     N = X.shape[0]
     
-    for i in range(N):
-        scores = np.dot(X[i,:], W)
-        true_score = scores[y[i]]
-        
-        # Calculate normalize probabilities
-        log_c = -np.max(scores)
-        sum_sj = np.sum(np.exp(scores + log_c))
-        
-        # Example specific loss and add to total
-        s_yi = true_score + log_c
-        L_i = -np.log(np.exp(s_yi)/sum_sj)
-        loss = loss + L_i
-        
-        # Calculate gradients for each class
-        for c in range(C):
-            # Pull score against all possible classes, apply activation
-            # and calculate derivative of softmax as dW
-            class_score = scores[c]
-            class_softmax = np.exp(class_score + log_c)/sum_sj
-            dW[:,c] = X[i,:]*(class_softmax-1*(c==y[i]))
-            
-    # normalize and regularize
-    dW = dW / N
-    loss = loss / N
+    dscores = np.zeros((N,C))
     
+    for i in range(N):
+        # Calculate the scores and subtract the max (predicted class)
+        scores = np.dot(X[i], W)
+        scores = scores - np.max(scores)
+        # Calculate probability w/ numerical stability trick
+        prob = np.exp(scores)/np.sum(np.exp(scores))
+        
+        binary_y = np.zeros(C)
+        # Subtract the probabilities from the true example class for gradient
+        binary_y[y[i]] = 1
+        dscores[i] = prob - binary_y
+        
+        # Calculate loss for the example, add to total
+        L_i = -np.log(prob[y[i]])
+        loss = loss + L_i
+    
+    # Normalize and regularize loss
+    loss = loss / N
     loss = loss + (0.5*reg*np.sum(W*W))
-    dW = dW + reg*W
+    
+    # Calculate gradient, normalize / regularize 
+    dW = np.dot(X.T, dscores)/N
+    dW = dW + (reg*W)
     
     pass
 
@@ -89,7 +87,28 @@ def softmax_loss_vectorized(W, X, y, reg):
     # regularization!                                                           #
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
-
+    N = X.shape[0]
+    C = W.shape[1]
+    
+    # Calculate scores and probability (increase dimension of max for broadcasting)
+    scores = np.dot(X, W)
+    scores = scores - np.max(scores, axis=1)[:, np.newaxis]
+    prob = np.exp(scores)/np.sum(np.exp(scores), axis=1)[:, np.newaxis]
+    
+    # Calculate and regularize loss
+    loss = -np.log(prob[np.arange(N), y])
+    loss = np.mean(loss)
+    loss = loss + (0.5*reg*np.sum(W*W))
+    
+    # Calculate score differences for gradient
+    dscores = prob
+    dscores[np.arange(N), y] = dscores[np.arange(N), y] - 1
+    dscores = dscores/N
+    
+    dW = np.dot(X.T, dscores)
+    dW = dW + (reg*W)
+    
+    
     pass
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
